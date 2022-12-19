40d4b31bb6a43c159382e70b45170d3fe843cc68
Iimanbek <imanbek.akylbekov2017@gmail.com>
2022-12-19 16:13:28 +0600

    filter done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,8 @@
 <template>
-  <navigation @CustomEventInputChanged="doSomething"></navigation>
+  <navigation></navigation>
   <main>
-    <select>
-      <option  value="all">-select the genre-</option>
-      <option  value="Romance">Romance</option>
-      <option  value="Drama">Drama</option>
-      <option  value="Science-Fiction">Science-Fiction</option>
-      <option  value="Thriller">Thriller</option>
-      <option  value="Action">Action</option>
-      <option  value="Crime">Crime</option>
-      <option  value="Horror">Horror</option>
-      <option  value="Mystery">Mystery</option>
-      <option  value="Adventure">Adventure</option>
-      <option  value="History">History</option>
-      <option  value="War">War</option>
+    <select class="selel" @change="filtera($event)">
+      <option v-for="option in options" :value="option.value">{{ option.text }}</option>
     </select>
     <div class="wrap-items">
       <cards :data="items"></cards>
@@ -28,18 +17,40 @@ export default {
   data() {
     return {
       items: null,
+      selectedOption: '',
+       options: [
+        { text: 'all', value: 'all' },
+        { text: 'Romance', value: 'Romance' },
+        { text: 'Drama', value: 'Drama' },
+        { text: 'Science', value: 'Science' },
+        { text: 'Thriller', value: 'Thriller' },
+        { text: 'Action', value: 'Action' },
+        { text: 'Crime', value: 'Crime' },
+        { text: 'Horror', value: 'Horror' },
+        { text: 'Mystery', value: 'Mystery' },
+        { text: 'History', value: 'History' },
+        { text: 'War', value: 'War' },
+        { text: 'Adventure', value: 'Adventure' }
+      ]
     }
   },
   components: {
     cards: Cards,
-
   },
   methods: {
     async getData() {
       const response = await fetch('https://api.tvmaze.com/shows')
       const data = await response.json()
       this.items = await data.filter(item => item.id <= 50)
-      // console.log(this.items);
+    },
+    async filtera(event) {
+      let value1 = event.target.value
+      console.log(value1);
+      await this.getData()
+      this.items = this.items.filter((item) => {
+      return item.genres.includes(value1);
+      });
+      console.log(this.items);
     },
   },
   mounted() {
@@ -48,6 +59,11 @@ export default {
 }
 </script>
 <style lang="css">
+.selel{
+  text-align: center;
+  display: flex;
+  margin-left: 45%;
+}
 .wrap-items {
   gap: 20px;
   padding: 30px 40px;
